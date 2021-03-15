94ecb77d68ac7dd4e3593891854538747ab01eb5
Deepak <Deepak@Ty-Deepak-Tr>
2021-03-15 12:54:47 +0530

    commit to git local

---
+++ SeleniumFramework_Sdet11/src/test/java/com/comcast/conatcttest/ContactTest.java
@@ -0,0 +1,21 @@
+package com.comcast.conatcttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	  @Test
+	  public void createconatcttest() {
+		  System.out.println("execute createconatcttest");
+	  }
+	  
+	  @Test
+	  public void modifyconatcttest() {
+		  System.out.println("execute modifyconatcttest");
+	  }
+	  
+	  @Test
+	  public void deleteconatcttest() {
+		  System.out.println("execute deleteconatcttest");
+	  }
+}
