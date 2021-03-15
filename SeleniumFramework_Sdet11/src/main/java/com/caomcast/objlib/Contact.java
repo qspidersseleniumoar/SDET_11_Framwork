84162833d703e7d492caf9711e85133c11247368
Deepak <Deepak@Ty-Deepak-Tr>
2021-03-15 13:41:22 +0530

    added new mtd by you

--- SeleniumFramework_Sdet11/src/main/java/com/caomcast/objlib/Contact.java
+++ SeleniumFramework_Sdet11/src/main/java/com/caomcast/objlib/Contact.java
@@ -1,12 +1,16 @@
 package com.caomcast.objlib;
 /**
  * 
- * @author user1
+ * @author user1/ deepak
  *
  */
 public class Contact {
 	
 	public void createConact(String conatctName) {
+		
+	}
+	
+	public void createConact(String conatctName , String mobile) {
 		
 	}
 
